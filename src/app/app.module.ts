0db9a569ccab514a2235ef6938a56490a32ee1e1
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2019-01-14 14:37:27 +0200

    Add: photo viewer and better styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { PhotoViewer } from '@ionic-native/photo-viewer';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  declarations: [MyApp, HomePage],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    PhotoViewer
   ]
 })
 export class AppModule {}
