32467ee3201ea9637580137e518735af257a74f0
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2019-02-09 14:43:17 +0200

    Add: correct status bar styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController, Platform } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { LoginPage } from '../pages/login/login';
 import { MenuPage } from '../pages/menu/menu';
+import { Cordova } from '@ionic-native/core';
 
 
 @Component({
@@ -23,7 +24,11 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+
+      if (platform.is('android')) {
+        statusBar.styleDefault();
+        statusBar.backgroundColorByHexString('#f8f8f8');
+      }
       splashScreen.hide();
     })
     .catch(err => {
